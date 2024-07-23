ac420876042f2793dd3e7912b528ec46c95430f3
Eric Li <icemastereric@gmail.com>
2024-07-22 21:54:04 -0600

    using llama-cpp-python for generation

--- waifumem/llm_gen.py
+++ waifumem/llm_gen.py
@@ -1,27 +1,32 @@
 from llama_cpp import Llama
 llm = Llama(
       model_path="waifumem/models/gemma-2-27b-it-Q5_K_L.gguf",
-      chat_format="gemma"
+      chat_format="gemma",
+      n_gpu_layers=-1
 )
-print(llm.create_chat_completion(
+for token in llm.create_chat_completion(
     messages = [
-        {"role": "system", "content": "You are a smart AI that summarizes conversations in a succinct and efficient manner. Summarize the following conversation."},
-        {"role": "User A", "content": "hey"},
-        {"role": "User B", "content": "yo"},
-        {"role": "User A", "content": "how's it going?"},
-        {"role": "User B", "content": "the craziest thing happened today actually"},
-        {"role": "User B", "content": "I actually got a gf"},
-        {"role": "User A", "content": "WHAT"},
-        {"role": "User C", "content": "THERE'S NO WAY"},
-        {"role": "User A", "content": "bro spill the beans rn"},
-        {"role": "User B", "content": "ok so basically in my ESL class"},
-        {"role": "User B", "content": "there's this really cute russian girl"},
-        {"role": "User B", "content": "and we somehow ended up talking and playing games together"},
-        {"role": "User A", "content": ":skull:"},
-        {"role": "User C", "content": "that doesn't make you boyfriend and girlfriend"},
-        {"role": "User B", "content": "I asked her out"},
-        {"role": "User B", "content": "and she agreed"},
-        {"role": "User A", "content": "unfriending you rn"},
-        {"role": "User C", "content": "nah bro you ain't my friend anymore"},
-    ]
-))
+        {"role": "user", "content": """You are a smart AI that summarizes the most relevant topics of a conversation. Return only the most important, relevant topics in the following conversation in a JSON list:
+Eric: hey
+Josh: yo
+Eric: how's it going?
+Josh: the craziest thing happened today, actually
+Josh: I actually got a gf
+Eric: WHAT
+Charlette: THERE'S NO WAY
+Josh: yep
+Eric: bro spill the beans rn
+Josh: ok so basically in my ESL class
+Josh: there's this really cute russian girl
+Josh: and we somehow ended up playing fireboy and watergirl together
+Eric: :skull:
+Charlette: bro are you 5 or something
+Josh: shut it
+Josh: not like any of you even talk to girls
+HydrogenMacro: girls? what are those?"""},
+    ], temperature=0.4, stream=True
+):
+    if token["choices"][0]["delta"].get("content"):
+        print(token["choices"][0]["delta"].get("content"), end="")
+
+print()
