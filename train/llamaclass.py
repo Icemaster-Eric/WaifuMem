36eb21f48ef48e593153c166998791e14b1a011c
Eric Li <icemastereric@gmail.com>
2024-07-26 17:41:22 -0600

    switched to exllamav2, still working on stuff

--- train/llamaclass.py
+++ train/llamaclass.py
@@ -1,71 +1,129 @@
-from typing import Optional, List
-from llama_cpp import Llama, LogitsProcessorList
-from lmformatenforcer import CharacterLevelParser, JsonSchemaParser
-from lmformatenforcer.integrations.llamacpp import build_llamacpp_logits_processor, build_token_enforcer_tokenizer_data
-from pydantic import BaseModel
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from exllamav2 import ExLlamaV2, ExLlamaV2Config, ExLlamaV2Cache, ExLlamaV2Tokenizer
+from exllamav2.generator import ExLlamaV2DynamicGenerator
+from exllamav2.generator.filters import ExLlamaV2PrefixFilter
+from lmformatenforcer.integrations.exllamav2 import ExLlamaV2TokenEnforcerFilter
+from lmformatenforcer import JsonSchemaParser
+from pydantic import BaseModel, conlist
+from typing import Literal
+import json
 
-"""llm = Llama(
-    model_path="train/Meta-Llama-3-70B-Instruct-IQ2_XS.gguf",
-    chat_format="llama-3",
-    n_ctx=8192,
-    n_gpu_layers=-1,
-)"""
+model_dir = "waifumem/models/llama-3.1-8b-instruct-exl2"
+config = ExLlamaV2Config(model_dir)
+config.arch_compat_overrides()
+model = ExLlamaV2(config)
+cache = ExLlamaV2Cache(model, max_seq_len = 32768, lazy = True)
+model.load_autosplit(cache, progress = True)
 
+print("Loading tokenizer...")
+tokenizer = ExLlamaV2Tokenizer(config)
 
-DEFAULT_SYSTEM_PROMPT = """You are a helpful AI assistant for classifying messages and outputting the results in JSON."""
+# Initialize the generator with all default parameters
 
+generator = ExLlamaV2DynamicGenerator(
+    model = model,
+    cache = cache,
+    tokenizer = tokenizer,
+)
 
-def get_prompt(message: str, system_prompt: str = DEFAULT_SYSTEM_PROMPT) -> str:
-    return f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+# JSON schema
 
-{system_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>
+class SuperheroAppearance(BaseModel):
+    title: str
+    issue_number: int
+    year: int
 
-{message}<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+class Superhero(BaseModel):
+    name: str
+    secret_identity: str
+    superpowers: conlist(str, max_length = 5)
+    first_appearance: SuperheroAppearance
+    gender: Literal["male", "female"]
+
+schema_parser = JsonSchemaParser(Superhero.schema())
+
+# Create prompts with and without filters
+
+def classify_prompt(message: str):
+    return """<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+
+You are an AI assistant that classifies whether a message should be remembered or not, and returns the output in JSON<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+Raiden is an infinitely more fun boss than signora<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+{"remember": true}<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+Hi Lucy goosey :goose:<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+{"remember": false}<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+Woah who were they?<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+{"remember": false}<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+Lmfao that's dark<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+{"remember": false}<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+artifact farming is hell on earth :sob:<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+{"remember": true}<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+That's a honkai star rail term<|eot_id|><|start_header_id|>assistant<|end_header_id|>
+
+{"remember": true}<|eot_id|><|start_header_id|>user<|end_header_id|>
+
+""" + message + """<|eot_id|><|start_header_id|>assistant<|end_header_id|>
 
 """
 
+i_prompts = [
+    classify_prompt("BROTHER THATS KACHINA :skull::skull: And Im matching with my best friends"),
+    classify_prompt("wtf you got a Nahida pfp, weren't you her biggest hater. what has happened to you"),
+    classify_prompt("nothing much, just relaxing n stuff"),
+    classify_prompt("Never tried it let me see"),
+    classify_prompt("Do I send my uid here"),
+]
 
-"""tokenizer_data = build_token_enforcer_tokenizer_data(llm)
+prompts = []
+filters = []
 
-def llamacpp_with_character_level_parser(prompt: str, character_level_parser: Optional[CharacterLevelParser]) -> str:
-    logits_processors: Optional[LogitsProcessorList] = None
-    if character_level_parser:
-        logits_processors = LogitsProcessorList([build_llamacpp_logits_processor(tokenizer_data, character_level_parser)])
-    
-    output = llm(prompt, logits_processor=logits_processors, max_tokens=100)
-    text: str = output['choices'][0]['text']
-    return text"""
+for p in i_prompts:
+    prompts.append(p)
+    filters.append(None)
+    prompts.append(p)
+    filters.append([
+        ExLlamaV2TokenEnforcerFilter(schema_parser, tokenizer),
+        ExLlamaV2PrefixFilter(model, tokenizer, ["{", " {"])
+    ])
 
+# Generate
 
-class AnswerFormat(BaseModel):
-    relevant: bool
+print("Generating...")
 
+outputs = generator.generate(
+    prompt = prompts,
+    filters = filters,
+    filter_prefer_eos = True,
+    max_new_tokens = 10,
+    add_bos = True,
+    stop_conditions = [tokenizer.eos_token_id],
+    completion_only = True
+)
 
-question = """Please classify the following. You MUST answer using the following json schema: """
-question_with_schema = f"{question}{AnswerFormat.schema_json()}"
-prompt = f"""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+# Print outputs:
 
-You are a helpful AI assistant for classifying messages and outputting the results in JSON. For each of the following messages, classify their relevance and give outputs<|eot_id|><|start_header_id|>user<|end_header_id|>
-
-question here<|eot_id|><|start_header_id|>assistant<|end_header_id|>
-
-response here<|eot_id|><|start_header_id|>user<|end_header_id|>
-
-question here<|eot_id|><|start_header_id|>assistant<|end_header_id|>
-
-response here<|eot_id|><|start_header_id|>user<|end_header_id|>
-
-question here<|eot_id|><|start_header_id|>assistant<|end_header_id|>
-
-response here<|eot_id|><|start_header_id|>user<|end_header_id|>
-
-question here<|eot_id|><|start_header_id|>assistant<|end_header_id|>
-
-response here<|eot_id|><|start_header_id|>user<|end_header_id|>"""
-print(prompt)
-
-#result = llamacpp_with_character_level_parser(prompt, JsonSchemaParser(AnswerFormat.schema()))
-
-
-#print(result)
+for i in range(len(i_prompts)):
+    print("---------------------------------------------------------------------------------")
+    print(i_prompts[i].strip())
+    print()
+    print("Without filter:")
+    print("---------------")
+    print(outputs[i * 2])
+    print()
+    print("With filter:")
+    print("------------")
+    print(json.dumps(json.loads(outputs[i * 2 + 1]), indent = 4).strip())
+    print()
