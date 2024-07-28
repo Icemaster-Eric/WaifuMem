8cc6e5fd9b84b0332f3279fadf4a6e80a89ebe2a
Eric Li <icemastereric@gmail.com>
2024-07-28 12:52:19 -0600

    start work on rvc + edge tts

--- tts/tts.py
+++ tts/tts.py
@@ -1,18 +1,18 @@
-from TTS.api import TTS
+import edge_tts
+from scipy.io.wavfile import write
+from tts.rvc.misc import load_hubert, get_vc, vc_single
 
 
 class RaineTTS:
     def __init__(self):
-        self.tts_model = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
+        self.hubert_model = load_hubert("tts/models/hubert_base.pt")
 
-    def tts(self, text: str, ref_wav: str, language: str = "en"):
-        self.tts_model.tts_to_file(text, speaker_wav=ref_wav, language=language, split_sentences=True)
+    def tts(self, text: str):
+        #get_vc(speaker_id, rvc_model_dir, 0.33, 0.5)
+        pass
 
 
 if __name__ == "__main__":
-    tts = RaineTTS()
+    raine_tts = RaineTTS()
 
-    tts.tts(
-        """I don't think SeamlessM4T qualifies as an end-to-end audio-to-audio model. The paper states "the task of speech-to-speech translation in SeamlessM4T v2 is broken down into speech-to-text translation (S2TT) and then text-to-unit conversion (T2U)". And while language translation is an important application as you mention, it's strictly limited to that. It wouldn't understand or produce non-speech audio (e.g. singing, music, environmental sounds, etc) and you can't have a conversation with it.""",
-        ref_wav="tts/ref.wav"
-    )
+    raine_tts.tts("Woah, so this is my new voice? Pretty cool.")
