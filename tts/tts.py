bc40c51a09f759ec6ac8caf9cc11ffdb65afada7
Eric Li <icemastereric@gmail.com>
2024-07-28 21:32:17 -0600

    more work on rvc integration

--- tts/tts.py
+++ tts/tts.py
@@ -1,15 +1,191 @@
 from styletts2 import tts
 from scipy.io.wavfile import write
-from tts.rvc.misc import load_hubert, get_vc, vc_single
+from tts.rvc.rtrvc import RVC
+from tts.rvc.configs.config import Config
+from tts.rvc.torchgate import TorchGate
 import sounddevice as sd
 import soundfile as sf
 import re
-import time
+from multiprocessing import Queue, Process
+import torch
+import torchaudio.transforms as tat
+import numpy as np
+
+
+class Harvest(Process):
+    def __init__(self, inp_q, opt_q):
+        Process.__init__(self)
+        self.inp_q = inp_q
+        self.opt_q = opt_q
+
+    def run(self):
+        import numpy as np
+        import pyworld
+
+        while 1:
+            idx, x, res_f0, n_cpu, ts = self.inp_q.get()
+            f0, t = pyworld.harvest(
+                x.astype(np.double),
+                fs=16000,
+                f0_ceil=1100,
+                f0_floor=50,
+                frame_period=10,
+            )
+            res_f0[idx] = f0
+            if len(res_f0.keys()) >= n_cpu:
+                self.opt_q.put(ts)
+
+
+class GUIConfig:
+    def __init__(self) -> None:
+        self.pth_path: str = ""
+        self.index_path: str = ""
+        self.pitch: int = 0
+        self.formant=0.0
+        self.sr_type: str = "sr_model"
+        self.block_time: float = 0.25  # s
+        self.threhold: int = -60
+        self.crossfade_time: float = 0.05
+        self.extra_time: float = 2.5
+        self.I_noise_reduce: bool = False
+        self.O_noise_reduce: bool = False
+        self.use_pv: bool = False
+        self.rms_mix_rate: float = 0.0
+        self.index_rate: float = 0.0
+        self.n_cpu: int = 2
+        self.f0method: str = "fcpe"
+        self.sg_hostapi: str = ""
+        self.wasapi_exclusive: bool = False
+        self.sg_input_device: str = ""
+        self.sg_output_device: str = ""
 
 
 class RaineTTS:
     def __init__(self):
         self.tts_model = tts.StyleTTS2()
+
+        # rvc stuff
+        self.config = Config()
+        self.gui_config = GUIConfig()
+        self.in_q = Queue()
+        self.out_q = Queue()
+        self.last_rvc = None
+    
+    def start_vc(self):
+        self.rvc = RVC(
+            3,
+            0.0,
+            "tts/models/kokomi/kokomi-rvc2.pth",
+            "tts/models/kokomi/kokomi-rvc2.index",
+            0.0,
+            2,
+            self.in_q,
+            self.out_q,
+            self.config,
+            last_rvc=self.last_rvc
+        )
+
+        self.gui_config.samplerate = (
+            self.rvc.tgt_sr
+            if self.gui_config.sr_type == "sr_model"
+            else self.get_device_samplerate()
+        )
+        self.gui_config.channels = self.get_device_channels()
+        self.zc = self.gui_config.samplerate // 100
+        self.block_frame = (
+            int(
+                np.round(
+                    self.gui_config.block_time
+                    * self.gui_config.samplerate
+                    / self.zc
+                )
+            )
+            * self.zc
+        )
+        self.block_frame_16k = 160 * self.block_frame // self.zc
+        self.crossfade_frame = (
+            int(
+                np.round(
+                    self.gui_config.crossfade_time
+                    * self.gui_config.samplerate
+                    / self.zc
+                )
+            )
+            * self.zc
+        )
+        self.sola_buffer_frame = min(self.crossfade_frame, 4 * self.zc)
+        self.sola_search_frame = self.zc
+        self.extra_frame = (
+            int(
+                np.round(
+                    self.gui_config.extra_time
+                    * self.gui_config.samplerate
+                    / self.zc
+                )
+            )
+            * self.zc
+        )
+        self.input_wav: torch.Tensor = torch.zeros(
+            self.extra_frame
+            + self.crossfade_frame
+            + self.sola_search_frame
+            + self.block_frame,
+            device=self.config.device,
+            dtype=torch.float32,
+        )
+        self.input_wav_denoise: torch.Tensor = self.input_wav.clone()
+        self.input_wav_res: torch.Tensor = torch.zeros(
+            160 * self.input_wav.shape[0] // self.zc,
+            device=self.config.device,
+            dtype=torch.float32,
+        )
+        self.rms_buffer: np.ndarray = np.zeros(4 * self.zc, dtype="float32")
+        self.sola_buffer: torch.Tensor = torch.zeros(
+            self.sola_buffer_frame, device=self.config.device, dtype=torch.float32
+        )
+        self.nr_buffer: torch.Tensor = self.sola_buffer.clone()
+        self.output_buffer: torch.Tensor = self.input_wav.clone()
+        self.skip_head = self.extra_frame // self.zc
+        self.return_length = (
+            self.block_frame + self.sola_buffer_frame + self.sola_search_frame
+        ) // self.zc
+        self.fade_in_window: torch.Tensor = (
+            torch.sin(
+                0.5
+                * np.pi
+                * torch.linspace(
+                    0.0,
+                    1.0,
+                    steps=self.sola_buffer_frame,
+                    device=self.config.device,
+                    dtype=torch.float32,
+                )
+            )
+            ** 2
+        )
+        self.fade_out_window: torch.Tensor = 1 - self.fade_in_window
+        self.resampler = tat.Resample(
+            orig_freq=self.gui_config.samplerate,
+            new_freq=16000,
+            dtype=torch.float32,
+        ).to(self.config.device)
+        if self.rvc.tgt_sr != self.gui_config.samplerate:
+            self.resampler2 = tat.Resample(
+                orig_freq=self.rvc.tgt_sr,
+                new_freq=self.gui_config.samplerate,
+                dtype=torch.float32,
+            ).to(self.config.device)
+        else:
+            self.resampler2 = None
+        self.tg = TorchGate(
+            sr=self.gui_config.samplerate, n_fft=4 * self.zc, prop_decrease=0.9
+        ).to(self.config.device)
+        self.start_stream()
+
+    def get_device_samplerate(self):
+        return int(
+            sd.query_devices(device=sd.default.device[0])["default_samplerate"]
+        )
 
     def tts(self, text: str):
         pattern = '|'.join('(?<={})'.format(re.escape(delim)) for delim in (". ", "? ", "! "))
@@ -19,28 +195,4 @@ class RaineTTS:
 
             self.tts_model.inference(sentence, output_wav_file="output.wav")
 
-            get_vc("kokomi-rvc2.pth", "tts/models/kokomi", 0.33, 0.5)
-
-            hubert_model = load_hubert("tts/models/hubert_base.pt")
-
-            wav_opt = vc_single(
-                0, 
-                "output.wav",
-                3, 
-                None, 
-                "harvest", 
-                "kokomi-rvc2",
-                '',
-                0.88,
-                3,
-                0,
-                1,
-                0.33,
-            )
-            print("done!")
-
-            write("output.wav", wav_opt[1][0], wav_opt[1][1])
-
-            sd.play(*sf.read("output.wav"))
-            sd.wait()
-            time.sleep(2)
+            # rvc stuff
