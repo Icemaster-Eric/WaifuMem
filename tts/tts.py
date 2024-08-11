9cba6911e1084c14aa416bbcffc42013f355260f
Eric Li <icemastereric@gmail.com>
2024-08-11 12:26:28 -0600

    setting up styletts2 through direct git clone of the repo

--- tts/tts.py
+++ tts/tts.py
@@ -2,7 +2,7 @@ import re
 import os
 import sounddevice as sd
 from scipy.io.wavfile import write
-from edge_tts import Communicate
+from StyleTTS2.ljinference import tts_to_file
 from tts.rvc.infer.modules.vc.modules import VC
 from tts.rvc.configs.config import Config
 
@@ -20,59 +20,24 @@ class RaineTTS:
         self.rvc_model.load_hubert("tts/models/hubert_base.pt")
 
     def tts(self, text: str):
+        tts_to_file(text, "output.wav", diffusion_steps=7, embedding_scale=1)
+
         output = self.rvc_model.vc_single(
-                0,
-                "rickroll vocals.m4a",
-                3,
-                0,
-                "rmvpe",
-                "tts/models/kokomi/kokomi-rvc2.index",
-                None,
-                0,
-                0.88,
-                0.33,
-                0.5,
-                0.33
-            )
+            0,
+            "output.wav",
+            3,
+            0,
+            "rmvpe",
+            "tts/models/kokomi/kokomi-rvc2.index",
+            None,
+            0,
+            0.88,
+            0.33,
+            0.5,
+            0.33
+        )
+
         write("output.wav", output[1][0], output[1][1])
-        return
-        pattern = "|".join(f"(?<={re.escape(delim)})" for delim in (". ", "? ", "! "))
 
-        for sentence in re.split(pattern, text):
-            communicate = Communicate(sentence, "en-US-AriaNeural")
-
-            subtitles: list[tuple[str, float, float]] = []
-
-            with open("output.mp3", "wb") as f:
-                for chunk in communicate.stream_sync():
-                    if chunk["type"] == "audio":
-                        f.write(chunk["data"])
-
-                    elif chunk["type"] == "WordBoundary":
-                        subtitles.append((
-                            chunk["text"],
-                            chunk["offset"] / 10000000,
-                            chunk["duration"] / 10000000
-                        ))
-
-            print(subtitles)
-
-            output = self.rvc_model.vc_single(
-                0,
-                "rickroll vocals.m4a",
-                3,
-                0,
-                "rmvpe",
-                "tts/models/kokomi/kokomi-rvc2.index",
-                None,
-                0,
-                0.88,
-                0.33,
-                0.5,
-                0.33
-            )
-
-            write("output.wav", output[1][0], output[1][1])
-
-            sd.play(output[1][1], output[1][0])
-            sd.wait()
+        #sd.play(output[1][1], output[1][0])
+        #sd.wait()
---
+++ tts_test.py
@@ -0,0 +1,8 @@
+from tts import RaineTTS
+
+
+tts = RaineTTS()
+
+tts.tts(
+    "StyleTTS 2: Towards Human-Level Text-to-Speech through Style Diffusion and Adversarial Training with Large Speech Language Models"
+)
